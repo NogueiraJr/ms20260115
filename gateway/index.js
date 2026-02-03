3808271574c8eb913d83b67eb97d0d88ff2b74d4
Nogueira Jr <nogueirajr@hotmail.com>
2026-02-03 14:51:10 -0300

    Refactor gateway service to load HELLO_SERVICE_URL from .env file

--- gateway/index.js
+++ gateway/index.js
@@ -1,23 +1,31 @@
 const express = require("express");
+
 const app = express();
+
+// lê variável de ambiente
+const HELLO_SERVICE_URL =
+  process.env.HELLO_SERVICE_URL || "http://localhost:3000";
 
 app.get("/hello", async (req, res) => {
   try {
-    const HELLO_SERVICE_URL =
-      process.env.HELLO_SERVICE_URL || "http://localhost:3000";
-
     const response = await fetch(`${HELLO_SERVICE_URL}/hello`);
     const data = await response.json();
+
+    console.log(
+      `[GATEWAY] chamada para ${HELLO_SERVICE_URL}/hello com sucesso`
+    );
+
     res.json({
       from: "gateway",
       data
     });
   } catch (err) {
+    console.error("[GATEWAY] erro ao chamar hello-service", err.message);
     res.status(500).json({ error: "Erro ao chamar hello-service" });
   }
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`gateway rodando na porta ${PORT}`);
+  console.log(`[GATEWAY] rodando na porta ${PORT}`);
 });
